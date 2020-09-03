5b483a6eebb100250e13ba3b280d4922e8f9e352
Joseph Hamman <jhamman@ucar.edu>
2020-09-02 21:07:48 -0700

    cleanup entrypoints

--- carbonplan/__init__.py
+++ carbonplan/__init__.py
@@ -1,15 +1,16 @@
 import sys
 
+import entrypoints
+
 imports = {}
 
 
 def __getattr__(attr):
-    print(attr)
+
     if attr == '__all__':
         return __dir__()
     try:
         module = imports[attr].load()
-        sys.modules[attr] = module
         sys.modules[f'carbonplan.{attr}'] = module
         return module
     except KeyError:
@@ -17,13 +18,10 @@ def __getattr__(attr):
 
 
 def __dir__(*_, **__):
-    print(_, __)
     return sorted(list(globals()) + list(imports))
 
 
 def _make_imports():
-    import entrypoints
-
     for module in entrypoints.get_group_all('carbonplan.modules'):
         imports[module.name] = module
 
