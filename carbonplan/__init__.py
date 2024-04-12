8c8bf042477e036d44879970a674ba7a2835d0aa
Max Jones <14077947+maxrjones@users.noreply.github.com>
2024-04-12 18:05:31 -0400

    Update pre-commit

--- carbonplan/__init__.py
+++ carbonplan/__init__.py
@@ -3,5 +3,5 @@ from pkg_resources import DistributionNotFound, get_distribution
 try:
     version = get_distribution(__name__).version
 except DistributionNotFound:  # pragma: no cover
-    version = '0.0.0'  # pragma: no cover
+    version = "0.0.0"  # pragma: no cover
 __version__ = version
